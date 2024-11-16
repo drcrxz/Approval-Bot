7ec9ea4cdeaef7e3c60a985d6add5025cb623e86
Elite Bots <cruzproject3301@gmail.com>
2024-11-16 17:41:26 +0530

    new

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello_world():
 if __name__ == "__main__":
     app.run()
 
-# Don't Remove Credit @VJ_Botz
+# Don't Remove Credit @Elites_info
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-# Don't Remove Credit @VJ_Botz
+# Don't Remove Credit @Elites_info
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
@@ -9,13 +9,13 @@ class Config:
     API_ID = int(getenv("API_ID", "20389440"))
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
     BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-    FSUB = getenv("FSUB", "VJ_Botz")
+    FSUB = getenv("FSUB", "Elites_info")
     CHID = int(getenv("CHID", "-1001623633000"))
     SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
 
-# Don't Remove Credit @VJ_Botz
+# Don't Remove Credit @Elites_info
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
